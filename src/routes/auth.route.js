f17131de049800b8df9cd85be1ed1167d73c825a
Victor98k <Victorgustav98@gmail.com>
2024-05-10 00:09:09 +0200

    trying to resolve render error.

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,11 @@
 //TODO add auth route
 const Express = require("express");
 const { registerUser, loginUser } = require("../controllers/auth.controller");
+const { authMiddleware } = require("../middlewares/auth.middleware");
 
 const authRouter = Express.Router();
 
-authRouter.post("/register", registerUser);
-authRouter.post("/login", loginUser);
+authRouter.post("/register", authMiddleware, registerUser);
+authRouter.post("/login", authMiddleware, loginUser);
 
 module.exports = authRouter;
